4349b7cd813f3e8a13b2c6a3e1330be1340d9c2e
Maahi2011 <89519001+Maahi2011@users.noreply.github.com>
2022-11-22 19:38:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//teachablemachine.withgoogle.com/models/cs1fzXy8W/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+})
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5library',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fDnaLf0P7/model.json',modelloaded);
+function modelloaded(){
+    console.log('model is loaded')
+}
